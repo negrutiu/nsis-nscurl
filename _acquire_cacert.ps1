2d297988d47b8d5e285e1ca1e87beb61501fc1b2
Marius Negruțiu <marius.negrutiu@protonmail.com>
2020-02-15 23:10:26 +0200

    Download cacert.pem

---
+++ _acquire_cacert.ps1
@@ -0,0 +1,30 @@
+$fn = "cacert.pem"
+$uri = "https://curl.haxx.se/ca/cacert.pem"
+Push-Location $PSScriptRoot
+
+if (ls $fn -ErrorAction:Ignore) {
+	$headers = @{ 'If-Modified-Since' = (ls $fn).LastWriteTimeUtc.ToString("ddd, dd MMM yyyy hh:mm:ss", [CultureInfo]::CreateSpecificCulture('en-US')) + " GMT" }
+}
+
+$response = try {
+    # Returns WebResponseObject. Convert to HttpWebResponse
+	(Invoke-WebRequest -Uri $uri -OutFile $fn -Headers $headers -PassThru).BaseResponse
+} catch {
+    # Returns HttpWebResponse
+    $_.Exception.Response
+}
+
+if (($response.StatusCode.value__ -ge 200) -and ($response.StatusCode.value__ -lt 300)) {
+    Write-Host -ForegroundColor green $response.StatusCode.value__ $response.StatusDescription
+} elseif (($response.StatusCode.value__ -ge 300) -and ($response.StatusCode.value__ -lt 400)) {
+    Write-Host -ForegroundColor yellow $response.StatusCode.value__ $response.StatusDescription
+} else {
+    Write-Host -ForegroundColor red $response.StatusCode.value__ $response.StatusDescription
+}
+$response.Headers.ToString()
+
+Remove-Variable headers -ErrorAction:Ignore
+#Remove-Variable response -ErrorAction:Ignore
+Pop-Location
+
+Pause
