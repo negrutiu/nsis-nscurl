0795e6dab96c6a0aaed8b7537d6cbdccecb9a202
Marius Negruțiu <marius.negrutiu@protonmail.com>
2020-02-15 22:08:32 +0200

    Small script fix

--- _build_Release_mingw.bat
+++ _build_Release_mingw.bat
@@ -16,7 +16,7 @@ call _acquire_pluginapi.bat
 if %errorlevel% neq 0 exit /B %errorlevel%
 
 :x86
-if not exist "%MINGW32%" echo ERROR: Missing "%MINGW32%" & pause & exit /B 2
+if not exist "%MINGW32%" echo ERROR: Missing "%MINGW32%" && pause && exit /B 2
 set PATH=%MINGW32%\bin;%ORIGINAL_PATH%
 
 echo.
@@ -26,7 +26,7 @@ echo %OUTDIR%
 title %OUTDIR%
 echo -------------------------------------------------------------------
 mingw32-make.exe ARCH=X86 CHAR=ANSI OUTDIR=%OUTDIR% -fMakefile.mingw clean all
-if %errorlevel% neq 0 echo errorlevel == %errorlevel% & pause & goto :EOF
+if %errorlevel% neq 0 echo errorlevel == %errorlevel% && pause && goto :EOF
 
 echo.
 echo -------------------------------------------------------------------
@@ -35,11 +35,11 @@ echo %OUTDIR%
 title %OUTDIR%
 echo -------------------------------------------------------------------
 mingw32-make.exe ARCH=X86 CHAR=Unicode OUTDIR=%OUTDIR% -fMakefile.mingw clean all
-if %errorlevel% neq 0 echo errorlevel == %errorlevel% & pause & goto :EOF
+if %errorlevel% neq 0 echo errorlevel == %errorlevel% && pause && goto :EOF
 
 
 :amd64
-if not exist "%MINGW64%" echo ERROR: Missing "%MINGW64%" & pause & exit /B 2
+if not exist "%MINGW64%" echo ERROR: Missing "%MINGW64%" && pause && exit /B 2
 set PATH=%MINGW64%\bin;%ORIGINAL_PATH%
 
 echo.
@@ -49,7 +49,7 @@ echo %OUTDIR%
 title %OUTDIR%
 echo -------------------------------------------------------------------
 mingw32-make.exe ARCH=X64 CHAR=Unicode OUTDIR=%OUTDIR% -fMakefile.mingw clean all
-if %errorlevel% neq 0 echo errorlevel == %errorlevel% & pause & goto :EOF
+if %errorlevel% neq 0 echo errorlevel == %errorlevel% && pause && goto :EOF
 
 echo.
 REM pause
