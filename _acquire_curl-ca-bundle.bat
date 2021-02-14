4760eccaa7e8095fbc28bea627e9a8f72b2109b4
Marius Negruțiu <marius.negrutiu@protonmail.com>
2021-02-14 08:52:39 +0200

    Updated cacert.pem download URL

--- _acquire_curl-ca-bundle.bat
+++ _acquire_curl-ca-bundle.bat
@@ -5,7 +5,7 @@ curl.exe -V > NUL 2> NUL
 if %errorlevel% neq 0 echo ERROR: Missing curl.exe && pause && exit /B 2
 
 set fn=%~dp0\curl-ca-bundle.crt
-set url=https://curl.haxx.se/ca/cacert.pem
+set url=https://curl.se/ca/cacert.pem
 echo Downloading %url% ...
 curl.exe -L -z "%fn%" -o "%fn%" --no-progress-meter -w "HTTP %%{response_code}" %url%
 echo.
