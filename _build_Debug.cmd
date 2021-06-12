a2a250c41bf91abac4c3c16dd2ca24794b8dc2e4
Marius Negruțiu <marius.negrutiu@bullguard.com>
2021-06-12 11:37:48 +0300

    VisualStudio 2019

--- _build_Debug.cmd
+++ _build_Debug.cmd
@@ -11,45 +11,22 @@ call _acquire_pluginapi.bat
 if %errorlevel% neq 0 exit /B %errorlevel%
 
 :DEFINITIONS
-if not exist "%PF%" set PF=%PROGRAMFILES(X86)%
-if not exist "%PF%" set PF=%PROGRAMFILES%
-
 set BUILD_SOLUTION=%CD%\NScurl.sln
 set BUILD_CONFIG=Debug
 set BUILD_VERBOSITY=normal
 :: Verbosity: quiet, minimal, normal, detailed, diagnostic
 
 :COMPILER
+if not exist "%PF%" set PF=%PROGRAMFILES(X86)%
+if not exist "%PF%" set PF=%PROGRAMFILES%
 set VSWHERE=%PF%\Microsoft Visual Studio\Installer\vswhere.exe
-if exist "%VSWHERE%" (
-	for /f "usebackq tokens=1* delims=: " %%i in (`"%VSWHERE%" -version 15 -requires Microsoft.Component.MSBuild`) do (
-		if /i "%%i"=="installationPath" (
-			set VCVARSALL=%%j\VC\Auxiliary\Build\VCVarsAll.bat
-			set BUILD_PLATFORMTOOLSET=v141
-		)
-	)
-)
-if exist "%VCVARSALL%" goto :BUILD
-
-set VCVARSALL=%PF%\Microsoft Visual Studio 14.0\VC\VcVarsAll.bat
-set BUILD_PLATFORMTOOLSET=v140
-if exist "%VCVARSALL%" goto :BUILD
-
-set VCVARSALL=%PF%\Microsoft Visual Studio 12.0\VC\VcVarsAll.bat
-set BUILD_PLATFORMTOOLSET=v120
-if exist "%VCVARSALL%" goto :BUILD
-
-set VCVARSALL=%PF%\Microsoft Visual Studio 11.0\VC\VcVarsAll.bat
-set BUILD_PLATFORMTOOLSET=v110
-if exist "%VCVARSALL%" goto :BUILD
-
-set VCVARSALL=%PF%\Microsoft Visual Studio 10.0\VC\VcVarsAll.bat
-set BUILD_PLATFORMTOOLSET=v100
-if exist "%VCVARSALL%" goto :BUILD
-
-echo ERROR: Can't find Visual Studio 2010/2012/2013/2015/2017
-pause
-exit /B 2
+if not exist "%VCVARSALL%" for /f "tokens=1* delims=: " %%i in ('"%VSWHERE%" -version 16 -requires Microsoft.Component.MSBuild 2^> NUL') do if /i "%%i"=="installationPath" set VCVARSALL=%%j\VC\Auxiliary\Build\VCVarsAll.bat&& set BUILD_PLATFORMTOOLSET=v142
+if not exist "%VCVARSALL%" for /f "tokens=1* delims=: " %%i in ('"%VSWHERE%" -version 15 -requires Microsoft.Component.MSBuild 2^> NUL') do if /i "%%i"=="installationPath" set VCVARSALL=%%j\VC\Auxiliary\Build\VCVarsAll.bat&& set BUILD_PLATFORMTOOLSET=v141
+if not exist "%VCVARSALL%" set VCVARSALL=%PF%\Microsoft Visual Studio 14.0\VC\VcVarsAll.bat&& set BUILD_PLATFORMTOOLSET=v140
+if not exist "%VCVARSALL%" set VCVARSALL=%PF%\Microsoft Visual Studio 12.0\VC\VcVarsAll.bat&& set BUILD_PLATFORMTOOLSET=v120
+if not exist "%VCVARSALL%" set VCVARSALL=%PF%\Microsoft Visual Studio 11.0\VC\VcVarsAll.bat&& set BUILD_PLATFORMTOOLSET=v110
+if not exist "%VCVARSALL%" set VCVARSALL=%PF%\Microsoft Visual Studio 10.0\VC\VcVarsAll.bat&& set BUILD_PLATFORMTOOLSET=v100
+if not exist "%VCVARSALL%" echo ERROR: Can't find Visual Studio 2010-2019 && pause && exit /B 2
 
 :BUILD
 pushd "%CD%"
