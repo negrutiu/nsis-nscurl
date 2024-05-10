29e1944c75321a0e61005e6cb6ba2bc6b8feefdf
Marius Negruțiu <marius.negrutiu@protonmail.com>
2024-05-10 19:19:08 +0300

    Download NSIS SDK from the official git

--- _acquire_pluginapi.bat
+++ _acquire_pluginapi.bat
@@ -1,10 +1,29 @@
+setlocal enabledelayedexpansion
 
-set NSIS_REPO=%~dp0..\..
+mkdir "%~dp0/nsis" > nul 2> nul
 
-xcopy "%NSIS_REPO%\Contrib\ExDLL\pluginapi.*"  "nsis\" /DYI
-xcopy "%NSIS_REPO%\Contrib\ExDLL\nsis_tchar.h" "nsis\" /DYI
-xcopy "%NSIS_REPO%\Source\exehead\api.h"       "nsis\" /DYI
-if not exist "nsis\*.*" if not exist "%NSIS_REPO%" echo ERROR: NSIS sources not found. Clone NSIS repository to "%NSIS_REPO%" && pause && exit /B 2
+set NSIS_GIT=%~dp0..\..
 
-echo.
-exit /B 0
+if exist "%NSIS_GIT%\Source\exehead\api.h" (
+    call :nsis_build
+) else (
+    call :standalone_build
+)
+exit /b
+
+REM -----------------------------------------------------------------------------------
+
+:nsis_build
+echo Copying NSIS SDK...
+xcopy "%NSIS_GIT%\Contrib\ExDLL\pluginapi.*"  "%~dp0\nsis\" /DYI || exit /b !errorlevel!
+xcopy "%NSIS_GIT%\Contrib\ExDLL\nsis_tchar.h" "%~dp0\nsis\" /DYI || exit /b !errorlevel!
+xcopy "%NSIS_GIT%\Source\exehead\api.h"       "%~dp0\nsis\" /DYI || exit /b !errorlevel!
+exit /b
+
+:standalone_build
+echo Downloading NSIS SDK...
+curl --no-progress-meter -L -o "%~dp0/nsis/pluginapi.h"  -z "%~dp0/nsis/pluginapi.h"   https://raw.githubusercontent.com/kichik/nsis/master/Contrib/ExDLL/pluginapi.h || exit /b !errorlevel!
+curl --no-progress-meter -L -o "%~dp0/nsis/pluginapi.c"  -z "%~dp0/nsis/pluginapi.c"   https://raw.githubusercontent.com/kichik/nsis/master/Contrib/ExDLL/pluginapi.c || exit /b !errorlevel!
+curl --no-progress-meter -L -o "%~dp0/nsis/nsis_tchar.h" -z "%~dp0/nsis/nsis_tchar.h"  https://raw.githubusercontent.com/kichik/nsis/master/Contrib/ExDLL/nsis_tchar.h || exit /b !errorlevel!
+curl --no-progress-meter -L -o "%~dp0/nsis/api.h"        -z "%~dp0/nsis/api.h"         https://raw.githubusercontent.com/kichik/nsis/master/Source/exehead/api.h || exit /b !errorlevel!
+exit /b
