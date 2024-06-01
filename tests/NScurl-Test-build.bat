c7eb6ef13fe748b7d24b16fda02b641c00ef760c
Marius Negruțiu <marius.negrutiu@protonmail.com>
2024-06-01 21:11:30 +0300

    Fixed the search for makensis.exe

--- tests/NScurl-Test-build.bat
+++ tests/NScurl-Test-build.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 if not exist "%NSIS%\makensis.exe" pushd "%~dp0..\.." && set NSIS=!cd!&& popd
 if not exist "%NSIS%\makensis.exe" set NSIS=%PROGRAMFILES%\NSIS
 if not exist "%NSIS%\makensis.exe" set NSIS=%PROGRAMFILES(X86)%\NSIS
-for /f "delims=*" %%f in ('where makensis.exe 2^> nul') do pushd "%%~dpf" && set NSIS=!cd!&& popd
+if not exist "%NSIS%\makensis.exe" for /f "delims=*" %%f in ('where makensis.exe 2^> nul') do pushd "%%~dpf" && set NSIS=!cd!&& popd
 if not exist "%NSIS%\makensis.exe" echo ERROR: NSIS not found&& pause && exit /b 2
 
 echo ********************************************************************************
