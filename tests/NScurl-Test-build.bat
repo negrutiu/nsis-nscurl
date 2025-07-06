b82ce0591a8bb5b14cc654cd06a9f1a8b5f74350
Marius Negruțiu <marius.negrutiu@protonmail.com>
2025-07-06 22:56:31 +0300

    Look for locally-built binaries when building examples

--- tests/NScurl-Test-build.bat
+++ tests/NScurl-Test-build.bat
@@ -16,6 +16,7 @@ echo.
 
 for %%t in (amd64-unicode x86-unicode x86-ansi) do (
     if exist "%nsisdir%\Stubs\lzma-%%t" (
+        if exist "%~dp0..\Release-mingw-%%t\NScurl.dll" set param_plugin_dir=/DPLUGIN_DIR=%~dp0..\Release-mingw-%%t
         if exist "%~dp0..\..\Plugins\%%t\NScurl.dll" set param_plugin_dir=/DPLUGIN_DIR=%~dp0..\..\Plugins\%%t
         title Build: %%t
         "%nsisdir%\makensis.exe" /V4 /DTARGET=%%t !param_plugin_dir! "%~dp0\NScurl-Test.nsi" || pause && exit /b !errorlevel!
