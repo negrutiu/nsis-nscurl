131a5c0422c69dc03a32ef796716fc6729a1fce3
Marius Negruțiu <marius.negrutiu@protonmail.com>
2026-04-21 20:05:06 +0300

    Minor `makensis.exe` detection improvement

--- tests/NScurl-Test.bat
+++ tests/NScurl-Test.bat
@@ -3,10 +3,15 @@ REM :: Marius Negrutiu :: 2019/08/25
 setlocal EnableDelayedExpansion
 
 rem | look upwards for makensis.exe
-for /f "usebackq delims=*" %%f in (`powershell -c "Get-Item '%~dp0' | ForEach-Object{ $d = $_.FullName; while($d){ $f = Join-Path $d 'makensis.exe'; if(Test-Path $f){ $f } $d = Split-Path $d }}"`) do if "!nsisdir!" equ "" set nsisdir=%%~dpf
+for /f "usebackq delims=*" %%d in (`powershell -ExecutionPolicy Bypass -Command "Get-Item '%~dp0' | ForEach-Object { $dir = $_.FullName; while($dir) { if (Test-Path (Join-Path $dir 'makensis.exe')) { $dir; break } $dir = Split-Path -Parent $dir }}"`) do set nsisdir=%%~d
 
 rem | makensis.exe search locations
 set PATH=%nsisdir%;%PATH%;%PROGRAMFILES%\NSIS;%PROGRAMFILES(X86)%\NSIS
+
+echo.
+echo -- where makensis
+where makensis
+echo.
 
 rem | for native amd64 installers, check out https://github.com/negrutiu/nsis
 for %%t in (x86-unicode x86-ansi amd64-unicode) do (
